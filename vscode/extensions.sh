1217d4887c91f341443dade2009a3e1ceed30c27
Felipe Fetter <felipetfetter@gmail.com>
2022-07-21 15:06:25 -0300

    add my vscode settings

---
+++ vscode/extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension golang.go
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension kiteco.kite
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension redhat.vscode-yaml
+code --install-extension mechatroner.rainbow-csv
